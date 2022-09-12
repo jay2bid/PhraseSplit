319817d1cc046b9c64a71daedafbfa15c731e376
jay2bid <jaybidokwu@gmail.com>
2022-09-12 14:12:03 +0300

    Initial Commit - First Test Pass

---
+++ PhraseSplitService.Tests/WordSplitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FluentAssertions;
+
+namespace PhraseSplitService.Tests
+{
+	public class WordSplitTests
+	{
+        private PhraseSplit _phraseSplit;
+
+        [SetUp]
+        public void Setup()
+        {
+            _phraseSplit = new PhraseSplit();
+        }
+
+        [Test]
+        public void Word_Split_Should_Return_Array_with_3_elements()
+        {
+            string[] splitphrase = { "how", "are", "you" };
+            string phrase = "how are you";
+            _phraseSplit.pharaseplitconverter("how are you").Should().BeEquivalentTo(splitphrase);
+        }
+
+
+        
+	}
+}
+
---
+++ PhraseSplitService/WordSplit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PhraseSplitService
+{
+	public class PhraseSplit
+	{
+		public string[] pharaseplitconverter(string phrase)
+
+		{
+			string[] splitphrase = { "how","are","you"};
+			return splitphrase;
+
+		}
+
+
+		public PhraseSplit()
+		{
+		}
+	}
+}
+
